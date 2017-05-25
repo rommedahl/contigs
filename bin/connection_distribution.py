994f22539093b67d1e33b016d2f65adac31fbbf5
NewlineCoding <magnus.brogie@gmail.com>
2017-05-25 12:36:13 +0200

    just beginning

---
+++ bin/connection_distribution.py
@@ -0,0 +1,11 @@
+import sys
+import bokeh
+
+#sys.argv is a list of arguments passed via the terminal to the script
+
+
+
+
+
+
+if __name__ == '__main__': #ensures that the main run isn't run when this file is importet
