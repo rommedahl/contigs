beccfa8204629220998eb5a9a827a48546d511fd
NewlineCoding <magnus.brogie@gmail.com>
2017-05-25 14:51:10 +0200

    just beginning

--- bin/connection_distribution.py
+++ bin/connection_distribution.py
@@ -17,33 +17,34 @@ if __name__ == '__main__': #ensures that the main run isn't run when this file i
     # mu, sigma = 0, 0.5
     # measured = np.random.normal(mu, sigma, 1000)
 
+    under_limit = 0
+    over_limit = 0
     data_list = []
     with open('overlaps.freq.txt') as oft:
         for line in oft:
             line_list = line.split(' ')
             value = int(line_list[0])
-            if value > 100:
+            if value < 20:
+                under_limit += 1
                 data_list += [int(line_list[0])]
+            else:
+                over_limit += 1
+
+    percentage_filtered = over_limit / (under_limit + over_limit)
+    print('% filtered {}'.format(percentage_filtered))
 
     measured = np.array(data_list)
     print(measured)
 
-    hist, edges = np.histogram(measured, density=True, bins=100)
+    hist, edges = np.histogram(measured, density=False, bins=100)
 
-    # x = np.linspace(-2, 2, 1000)
-    # pdf = 1/(sigma * np.sqrt(2*np.pi)) * np.exp(-(x-mu)**2 / (2*sigma**2))
-    # cdf = (1+scipy.special.erf((x-mu)/np.sqrt(2*sigma**2)))/2
-
+    x = np.logspace(0, 4000, 1000)
     p1.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:],
             fill_color="#036564", line_color="#033649")
-    # p1.line(x, pdf, line_color="#D95B43", line_width=8, alpha=0.7, legend="PDF")
-    # p1.line(x, cdf, line_color="white", line_width=2, alpha=0.7, legend="CDF")
 
 
     p1.legend.location = "top_left"
     p1.xaxis.axis_label = 'x'
     p1.yaxis.axis_label = 'Pr(x)'
-
-    # show(gridplot(p1, ncols=2, plot_width=400, plot_height=400, toolbar_location=None))
 
     bp.show(p1)
