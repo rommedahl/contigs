5350b8cca2e83dbc6aa56250f8a21e88928ff099
michaelstaahle <michael.staahle@gmail.com>
2017-05-25 16:27:48 +0200

    Förenklingar

--- dataAnalysis.sh
+++ dataAnalysis.sh
@@ -8,19 +8,16 @@ start=`date +%s`
 
 cat $1 |
 	tee >(wc -l > $1.rows.N) |	# Antal rader i filen
-uniq |
-	tee >(wc -l > $1.rows.unique.N) >(awk '{print $1"	"$2}' | uniq | wc -l > $1.overlaps.presort.unique.N ) |	# Antal unika rader. Antal unika ID-rader.
-awk '{if ($1>$2) print $2"	"$1; else print $1"	"$2}' |
-sort --parallel=6 |
-	tee >(wc -l > $1.overlaps.N) >(uniq | wc -l > $1.overlaps.unique.N) >(uniq -d > $1.overlaps.duplicates) |	# Antal unika kopplingar mellan kontiger
-awk '{OFS="\n"};{print $1,$2}' | sort --parallel=6 | uniq -c | 
-	tee >(sort -nr | awk '{print $2" "$1}' > $1.IDs.freq) |	# Tabell över förekomster av varje contig
-awk '{print $2}' | uniq |
-	tee >(cat > $1.IDs.unique) | # Unika contig ID
+awk '{print $1"	"$2}' |	# Behåller kolumn 1 och 2
+	tee >(uniq | wc -l > $1.overlaps.unique.N) >(uniq -d > $1.overlaps.duplicates) |	# Antal unika kopplingar mellan kontiger samt en lista på överlapp som det finns dubletter av
+awk '{OFS="\n"};{print $1,$2}' | sort --parallel=4 | uniq -c |	# Radbryter rader så att det blir en sorterad kolumn och summerar en frekvenstabell över förekomster av varje contig
+	tee >(sort -nr | awk '{print $1"	"$2}' > $1.IDs.freq) |	#  Sorterar frekvenstabellen och skickar till fil
+awk '{print $2}' | uniq |	# Skickar enbart ID-kolumnen
+	tee >(cat > $1.IDs.unique) |	# Unika contig ID
 wc -l > $1.IDs.unique.N	# Antal unika contig ID
 
 # Tidsåtgång, verkar ej fungera
 end=`date +%s`
 runtime=$((end-start))
-echo $runtime minuter> $1.runtime
+echo $runtime sekunder> $1.rtime
 
