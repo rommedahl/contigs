9d878e5c54e236b588fc5fda097e2166d879391c
walterberge <walter.n.berge@gmail.com>
2017-05-26 15:02:44 +0200

    Unit testing for graph algorithm

--- bin/test_graph_representation.py
+++ bin/test_graph_representation.py
@@ -15,13 +15,13 @@ class TestVertex:
             assert int(chr) in neighbour_list
 
 class TestGraph:
-    def test_graph_constructor(self):               # creates a graph with 4 vertices
-        graph_dict = {}                             # ensures all vertices are entered into graph
-        graph_dict['v1'] = {'v2', 'v3'}             # also tests subtree algorithms for subgraphs
-        graph_dict['v2'] = {'v1'}                   # ensures correct vertices are entered into different subgraphs
-        graph_dict['v3'] = {'v1'}                   # v2 -- v1 -- v3   v4
-        graph_dict['v4'] = {}                       # two subgraphs
-        graph = Graph(graph_dict)
+    def test_graph_constructor(self):
+            graph_dict = {}
+            graph_dict['v1'] = {'v2', 'v3'}
+            graph_dict['v2'] = {'v1'}
+            graph_dict['v3'] = {'v1'}
+            graph_dict['v4'] = {}
+            graph = Graph(graph_dict)
         vertices = graph.vertex_dictionary
         assert 'v1' and 'v2' and 'v3' and 'v4' in vertices
         graph.create_subgraph_dict()
