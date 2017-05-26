7ee945747b024516388535a19c9dbccd0b0c5f3a
Marcus Rommedahl <development@rommedahl.net>
2017-05-26 22:14:32 +0200

    Updated contigData after some problem with git

--- bin/shellscripts/contigData.sh
+++ bin/shellscripts/contigData.sh
@@ -1,40 +1,6 @@
 #!/bin/bash
-# File to sort the .m4 file so that every row contains unique data and outputs to a file.
+# From a M4 it returns a table with contig ID, its number of occurences and its length.
+# Input:  stdin in M4 file format
+# Output: stout
+{ cut -f1,8 $1; cut -f2,12 $1; } | sort | uniq -c | sort -nr | awk '{print $2"	"$1"	"$3}'
 
-# TODO. Tee all information to variables that is later put into a summary file instead of separate files.
-
-
-start=`date +%s`	# Start tidtagning
-
-{ cut -f1,8 $1; cut -f2,12 $1; } | sort | uniq -c | sort -nr | cut -f1 #$1.contigData
-
-# Tidsåtgång
-end=`date +%s`
-runtime=$((end-start))
-rumtimeM=$((runtime / 60))
-echo $runtime seconds > $1.nDest.rTime
-echo 'Script is completed. Duration '$rumtimeM' minutes.'
-spd-say -i -50 -p -100 -t male2 'Script is completed. Duration '$rumtimeM' minutes.'
-
-
-
-
-
-
-
-
-
-exit
-
-
-if [ ! -f $1.ndest ]; then	# If the output file does not exist
-	cat $1 |	# Cat the stdin
-		tee >(wc -l > $1.N) |	# Print the number of lines to file
-	uniq |	# Remove identical rows
-		tee >(wc -l > $1.N.uniq) |	# Print number of identical lines to file
-	#Sort the fields within each row for unique representation
-	awk '{if ($1<$2) print $1"	"$2"	"$3"	"$4"	"$5"	"$6"	"$7"	"$8"	"$9"	"$10"	"$11"	"$12; else print $2"	"$1"	"$3"	"$4"	"$5"	"$10"	"$11"	"$12"	"$9"	"$6"	"$7"	"$8}' |
-	sort --parallel=6 -u |	# Sort rows and make every row unique
-		tee >(wc -l > $1.nDest.N) |	# Print number of lines of new dataset to file
-	cat > $1.nDest	# Cat the output to file
-fi
