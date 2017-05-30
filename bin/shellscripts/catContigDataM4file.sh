67ab33ffd2300c7e8ccd9ef0cb2b9d17007efa09
Marcus Rommedahl <development@rommedahl.net>
2017-05-30 19:35:03 +0200

    Updated, renamed and removed bash scripts

--- bin/shellscripts/catContigDataM4file.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# From a M4 it returns a table with contig ID, its number of occurences and its length.
-# Input:  stdin in M4 file format
-# Output: stout
-{ cut -f1,8 $1; cut -f2,12 $1; }
