1f47add608bda7a8d7b526f9be04b58dcd909e03
Marcus Rommedahl <development@rommedahl.net>
2017-06-05 11:30:20 +0200

    Added, again, the Bashscripts that are used by main.

--- bin/shellscripts/superContigsM4.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Keeps overlaps when one contig is covered by the other contig.
-#
-# Input:  Standard input with file or stream of .m4 format.
-# Output: Standard output
-#
-awk '{if ($6 == 0 && $7 == $8) print $2; else if ($10 == 0 && $11 == $12) print $1;}' $1
-
